2f86e014e1c5d009922193df9357b83e8d5e4124
Rod_dev <95889654+rodrigoSouza1984@users.noreply.github.com>
2025-07-26 08:58:10 -0300

    Update bancoDeDadosConect.js

--- repositories/bancoDeDadosConect.js
+++ repositories/bancoDeDadosConect.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize'
 
 const sequelize = new Sequelize(
-    "postgres://oewvfpwv:IOedSOjvoyjzS1wEfSxsAW2qPkn_tSFj@ziggy.db.elephantsql.com/oewvfpwv",   
+    "url conection - postgres://...",   
     {
         dialect: "postgres",								      
         define: {									       
